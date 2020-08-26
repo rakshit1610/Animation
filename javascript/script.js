27389ddcbde56758c24aede2ae3115f2b4e547df
rakshit1610 <rakshitpanjwani@gmail.com>
2020-08-26 12:11:43 +0530

    added rotating motion to astronaut

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
 var astronaut= document.getElementById("astronaut");
+var ptr=0;
 var left= astronaut.offsetLeft;
 var tp=astronaut.offsetTop;
 var moveLeft=true;
@@ -8,20 +9,30 @@ setInterval(function(){
     left++;
 
     if(tp>(window.innerHeight)){
+        ptr+=0.25;
+        astronaut.style.transform= `rotate(${ptr}deg)`;
         moveLeft=false;
     }
     if(tp<=0){
+        ptr+=0.5;
+        astronaut.style.transform= `rotate(${ptr}deg)`;
         moveLeft=true;
     }
 
     if(moveLeft){
+        ptr+=0.5;
+        astronaut.style.transform= `rotate(${ptr}deg)`;
         tp++;
     }
     else{
+        ptr+=0.5;
+        astronaut.style.transform= `rotate(${ptr}deg)`;
         tp--;
     }
 
     if(left>window.innerWidth){
+        ptr+=0.5;
+        astronaut.style.transform= `rotate(${ptr}deg)`;
         left=0;
         i=Math.floor(Math.random()*3)+1;
         document.getElementById("bg").style.backgroundImage=`url(images/space${[i]}.jpg)`;
